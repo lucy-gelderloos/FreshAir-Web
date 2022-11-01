f001cd13fe3445daba0ca6911e4156854037b99f
Lucy Gelderloos <lucygel@gmail.com>
2022-10-31 18:38:06 -0700

    Initialize

---
+++ src/main/java/com/gelderloos/freshair/FreshairApplication.java
@@ -0,0 +1,13 @@
+package com.gelderloos.freshair;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreshairApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreshairApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gelderloos/freshair/FreshairApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gelderloos.freshair;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreshairApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
