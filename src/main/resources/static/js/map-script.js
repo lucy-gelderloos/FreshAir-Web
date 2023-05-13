bc0463aedaf85e6316a5831dc4bb70263a3d3ffa
Lucy Gelderloos <lucygel@gmail.com>
2023-05-12 17:40:05 -0700

    Create getStationsFromBounds

--- src/main/resources/static/js/map-script.js
+++ src/main/resources/static/js/map-script.js
@@ -29,6 +29,69 @@ if(!localStorage.getItem('currentZoom')) {
 
 const formInputUserName = document.getElementById('formInputUserName');
 
+function getStationsFromBounds(boundsRaw) {
+  const coordRegex = new RegExp("[\(\(](-?[0-9]{1,3}\.[0-9]{6})[0-9]*, (-?[0-9]{1,3}\.[0-9]{6})[0-9]*[\)], [\(](-?[0-9]{1,3}\.[0-9]{6})[0-9]*, (-?[0-9]{1,3}\.[0-9]{6})[0-9]*[\)\)]");
+  currentBounds = boundsRaw.match(coordRegex)[2] + ',' + boundsRaw.match(coordRegex)[1] + ',' + boundsRaw.match(coordRegex)[4] + ',' + boundsRaw.match(coordRegex)[3];
+  formInputBounds.value = currentBounds;
+
+        const baseUrl = "http://localhost:8080/visible-stations/";
+        const fullUrl = baseUrl + currentBounds;
+
+        console.log(fullUrl);
+
+        fetch(fullUrl)
+        .then((response) => response.json())
+        .then((data) => {
+
+        let stationsArr = [];
+        let shortestDistance = 0;
+
+    //    create array of stations
+          data.forEach(d => {
+    //        console.log(d);
+            let station = JSON.parse(d);
+            stationsArr.push(station);
+          });
+
+    //    find smallest distance from user
+          stationsArr.forEach(s => {
+            if(s.distanceFromUser < shortestDistance) {
+                shortestDistance = s.distanceFromUser;
+            }
+          });
+
+    //    create station markers
+          stationsArr.forEach(s => {
+                  let stationInfoString = "AQI: " + s.currentAQI + " (" + s.aqiDesc + ")";
+                  let iw = new google.maps.InfoWindow({
+                    content: stationInfoString
+                  });
+                  const stationMarker = document.createElement("div");
+                  stationMarker.classList.add(s.aqiDesc,"stationMarker");
+                  if(s.distanceFromUser == shortestDistance) {stationMarker.classList.add("closest")};
+                  stationMarker.textContent = s.currentAQI;
+
+                  let marker = new google.maps.marker.AdvancedMarkerView({
+                    position: {lat: Number(s.lat), lng: Number(s.lon)},
+                    map: map,
+                    title: s.siteName,
+                    content: stationMarker,
+                  });
+                  marker.addEventListener("gmp-click", () => {
+                    iw.open({
+                      anchor: marker,
+                      map: map,
+                    });
+                  });
+          });
+
+
+
+          });
+
+}
+
+
 // Initialize and add the map
 function initMap() {
 
@@ -51,76 +114,22 @@ function initMap() {
     userMarker.setPosition(currentCenter);
     localStorage.setItem('currentCenter',JSON.stringify(currentCenter));
     formInputCenter.value = currentCenter.lat() + "," + currentCenter.lng();
+    let boundsRaw = map.getBounds().toString();
+    getStationsFromBounds(boundsRaw);
   });
 
   map.addListener("zoom_changed", () => {
-      currentZoom = map.getZoom();
-      localStorage.setItem('currentZoom',currentZoom);
+    currentZoom = map.getZoom();
+    localStorage.setItem('currentZoom',currentZoom);
+    let boundsRaw = map.getBounds().toString();
+    getStationsFromBounds(boundsRaw);
   });
 
-// when bounds change, send a request with new bounds to /visible-stations, then
-  map.addListener("bounds_changed", () => {
+  map.addListener("dragend", () => {
     let boundsRaw = map.getBounds().toString();
-    const coordRegex = new RegExp("[\(\(](-?[0-9]{1,3}\.[0-9]{6})[0-9]*, (-?[0-9]{1,3}\.[0-9]{6})[0-9]*[\)], [\(](-?[0-9]{1,3}\.[0-9]{6})[0-9]*, (-?[0-9]{1,3}\.[0-9]{6})[0-9]*[\)\)]");
     currentBounds = boundsRaw.match(coordRegex)[2] + ',' + boundsRaw.match(coordRegex)[1] + ',' + boundsRaw.match(coordRegex)[4] + ',' + boundsRaw.match(coordRegex)[3];
-    formInputBounds.value = currentBounds;
-
-    const baseUrl = "http://localhost:8080/visible-stations/";
-    const fullUrl = baseUrl + currentBounds;
-
-    console.log(fullUrl);
-
-    fetch(fullUrl)
-    .then((response) => response.json())
-    .then((data) => {
-
-    let stationsArr = [];
-    let shortestDistance = 0;
-
-//    create array of stations
-      data.forEach(d => {
-//        console.log(d);
-        let station = JSON.parse(d);
-        stationsArr.push(station);
-      });
-
-//    find smallest distance from user
-      stationsArr.forEach(s => {
-        if(s.distanceFromUser < shortestDistance) {
-            shortestDistance = s.distanceFromUser;
-        }
-      });
-
-//    create station markers
-      stationsArr.forEach(s => {
-              let stationInfoString = "AQI: " + s.currentAQI + " (" + s.aqiDesc + ")";
-              let iw = new google.maps.InfoWindow({
-                content: stationInfoString
-              });
-              const stationMarker = document.createElement("div");
-              stationMarker.classList.add(s.aqiDesc,"stationMarker");
-              if(s.distanceFromUser == shortestDistance) {stationMarker.classList.add("closest")};
-              stationMarker.textContent = s.currentAQI;
-
-              let marker = new google.maps.marker.AdvancedMarkerView({
-                position: {lat: Number(s.lat), lng: Number(s.lon)},
-                map: map,
-                title: s.siteName,
-                content: stationMarker,
-              });
-              marker.addListener("click", () => {
-                iw.open({
-                  anchor: marker,
-                  map: map,
-                });
-              });
-      });
-
-
-
-      });
+    getStationsFromBounds(boundsRaw);
     });
-
 }
 
 window.initMap = initMap;
